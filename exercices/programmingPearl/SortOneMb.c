909e92ed4b4d072ee1597940b2b45c608a40edaf
Lucas <biblion84@users.noreply.github.com>
2021-11-15 15:32:01 +0100

    Exercice 4 column 1

--- exercices/programmingPearl/SortOneMb.c
+++ exercices/programmingPearl/SortOneMb.c
@@ -5,18 +5,6 @@
 #define LINES 3410000
 #define MAX_LINE 10000000
 
-unsigned int seed;
-
-/* The state word must be initialized to non-zero */
-unsigned int random()
-{
-	/* Algorithm "xor" from p. 4 of Marsaglia, "Xorshift RNGs" */
-	unsigned int x = seed;
-	x ^= x << 13;
-	x ^= x >> 17;
-	x ^= x << 5;
-	return seed = x;
-}
 
 
 void createFile(){
@@ -28,32 +16,16 @@ void createFile(){
 			file[fileIndex++] = i;
 		}
 	}
-	/* Column 1 exercice 4
-		Generating k integers less than n without duplicates.
-		* Step 1 : Generate integer with an incrementing index from 0 to n
-		* Step 2 : shuffle
-		* Step 3 : ??
-		* Step 4 : Profit
-		*/
-	seed = 22;
-	 //shuffle the data around
-	for (int i = 0; i < (fileIndex / 2); i++) {
-		int swapIndex = (random() % (fileIndex / 2)) + fileIndex/2;
-		if (swapIndex != i) {
-			int temp = file[i];
-			file[i] = file[swapIndex];
-			file[swapIndex] = temp;
-		}
-	}
 	FILE *f = fopen("C:/cbuild/1", "wb");
-	fwrite(file, sizeof(int), fileIndex, f);
+	fwrite(file, sizeof(int), LINES, f);
 	fclose(f);
-	free(file);
 }
 
 
 int main(int argc, char *argv[]) {
+	printf("%d\n", RAND_MAX);
 	createFile();
+	printf("File written\n");
 	
 	unsigned char *bitfield = (unsigned char*)malloc((MAX_LINE / 8) * sizeof(unsigned char)); // One bit per value
 	{
@@ -62,40 +34,59 @@ int main(int argc, char *argv[]) {
 			bitfield[i] = 0;
 		}
 		int x = 0;
-		while (fread(&x, sizeof(int), 1, f) && !feof(f) && x >= 0) {
+		int read = 0;
+		int maxIndex = 0;
+		int maxX = 0;
+		while (fread(&x, sizeof(int), 1, f) && !feof(f)) {
+			if (x < 0){
+				break;
+			}
 			int index = x / 8;
+			if (index > maxIndex) maxIndex = index;
+			if (x > maxX) maxX = x;
 			int rest = x % 8;
+			++read;
 			unsigned char bitcompare = (1 << rest);
+			
 			bitfield[index] = (bitfield[index] | bitcompare);
 		}
-		printf("%d\n", x);
+		printf("Max index: %d\n", maxIndex);
+		printf("Max number: %d\n", maxX);
+		
+		printf("Read: %d\n", read);
 		if (ferror(f)) {
 			printf("error\n");
 		}
 	}
 	{
 		FILE *output = fopen("C:/cbuild/output", "wb");
+		int c = 0;
 		for (int i = 0; i < MAX_LINE; ++i) {
 			int index = i / 8;
 			int rest = i % 8;
 			unsigned char bitcompare = (1 << rest);
+			//printf("index : %3d, rest : %3d, bitcompare : %3d, bitfield : %3d\n", index, rest, bitcompare, bitfield[index]);
+			int toWrite = i;
 			if (bitfield[index] & bitcompare) {
-				fwrite(&i, sizeof(int), 1, output);
+				printf("%d\n", i);
+				fwrite(&toWrite, sizeof(int), 1, output);
+				++c;
 			}
 		}
 		fclose(output);
+		printf("%d\n", c);
+	}
+	{
+		FILE *readOutput = fopen("C:/cbuild/output", "rb");
+		int y = 0;
+		while (fread(&y, sizeof(int), 1, readOutput) && !feof(readOutput)) {
+			//printf("%d\n", y);
+			++y;
+		}
+		printf("Number written : %d\n", y);
 	}
 	
-	{
-		FILE *reader = fopen("C:/cbuild/output", "rb");
-		int readInt;
-		while (fread(&readInt, sizeof(int), 1, reader) && !feof(reader)) {
-			printf("%d\n", readInt);
-		}
-		fclose(reader);
-	}
-
 	printf("Ok\n");
-
+	
 }
 
