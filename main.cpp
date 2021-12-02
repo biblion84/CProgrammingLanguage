dc0276d98ac6b7e50d1471bdfed1df93b147afc0
Lucas <biblion84@users.noreply.github.com>
2021-12-02 14:54:39 +0100

    Quick gen

--- main.cpp
+++ main.cpp
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void knuthGen() {
+// O(n) with n
+void knuthGen(int m,int n) {
 	srand(12);
 
-	int m  = 10;
-	int n = 100;
+	int total = 0;
 	for (int i = 0; i < n; i++){
 		int random = rand();
 		int modulo = (n-i);
-		printf("chance to pick : %.2f\n", (float)m/(float)(n-i) );
 		if ((random % modulo) < m ) {
-			printf("%d\n", i);
+			total = (total + i) % 13;
 			m--;
 		}
 	}
+	printf("%d\n", total);
 }
 
-void shuffleRand() {
+// O(n) with n
+void shuffleRand(int m, int n) {
 	srand(12);
-	int m  = 10;
-	int n = 100;
 
 	int *arr = (int *)malloc(n * sizeof(int));
 	for (int i = 0; i < n; i++) {
@@ -33,8 +32,50 @@ void shuffleRand() {
 		arr[i] = arr[nIndex];
 		arr[nIndex] = temp;
 	}
-
+	int total = 0;
 	for (int i = 0; i < m; i++){
-		printf("%d\n", arr[i]);
+		total = (total + arr[i]) % 13;
 	}
+	printf("%d\n", total);
+}
+
+void set(unsigned char *bitmap, int n){
+	int index = n / 8;
+	int rest = n % 8;
+	unsigned char bitcompare = (1 << rest);
+	bitmap[index] = (bitmap[index] | bitcompare);
+}
+
+bool test(unsigned char *bitmap, int n) {
+	int index = n / 8;
+	int rest = n % 8;
+	unsigned char bitcompare = (1 << rest);
+	return bitmap[index] & bitcompare;
+}
+
+// O(n) with m ?
+void quickGen(int m, int n) {
+	srand(12);
+
+	unsigned char *bitfield = (unsigned char *)malloc((n / 8) * sizeof(unsigned char));
+	int found = 0;
+	int total = 0;
+	while(found < m) {
+		int next = rand() % n;
+		if (!test(bitfield, next)) {
+			set(bitfield, next);
+			found++;
+			total = (total + next) % 13;
+		}
+	}
+	printf("%d\n", total);
+}
+
+int main() {
+	quickGen(1000, 1000000);
+	quickGen(10000, 10000000);
+	quickGen(100, 100002300);
+	quickGen(10300, 1011200000);
+	quickGen(12000, 1000000);
+	quickGen(10, 1000000000);
 }
