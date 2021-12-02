d03ec87430995a9ca6adbe083c29d9049b480193
Lucas <biblion84@users.noreply.github.com>
2021-12-02 14:33:21 +0100

    Shuffle rand

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+void knuthGen() {
 	srand(12);
 
 	int m  = 10;
@@ -14,5 +14,27 @@ int main() {
 			printf("%d\n", i);
 			m--;
 		}
+	}
+}
+
+void shuffleRand() {
+	srand(12);
+	int m  = 10;
+	int n = 100;
+
+	int *arr = (int *)malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++) {
+		arr[i] = i + 1;
+	}
+
+	for (int i = 0; i < n; i++) {
+		int nIndex = rand() % n;
+		int temp = arr[i];
+		arr[i] = arr[nIndex];
+		arr[nIndex] = temp;
+	}
+
+	for (int i = 0; i < m; i++){
+		printf("%d\n", arr[i]);
 	}
 }
