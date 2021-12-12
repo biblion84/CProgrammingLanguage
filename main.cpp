52055b4b8ea5d878c5c442da146718ce2f710d44
Lucas <biblion84@users.noreply.github.com>
2021-12-12 16:15:44 +0100

    Advent of code 12, scrap the tree idea, try the stack one

--- main.cpp
+++ main.cpp
@@ -1,130 +1,133 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-struct node {
-	int value;
-	node *next;
-};
+const int CONNECTION_SIZE = 10;
 
 
-node *append(node *l, node *n) {
-	n->next = l;
-	return n;
+//struct cave {
+//	char *id;
+//	cave **connections;
+//};
+//
+//cave caves[100] = {};
+//int cavesIdx = 0;
+//
+//cave *newcave(char *id) {
+//	cave *n = &caves[cavesIdx++];
+//	n->id = id;
+//	n->connections = (cave **)malloc(sizeof(cave *) * CONNECTION_SIZE);
+//	return n;
+//}
+//
+//cave *find(char *id) {
+//	for (int i = 0; i < 100 && caves[i].id != 0; i++) {
+//		if (strcmp(caves[i].id, id) == 0) { 
+//			return &caves[i];
+//		}
+//	}
+//	return 0;
+//}
+//
+// does not check for duplicates
+//void addconnection(cave *from, cave *to) {
+//	for (int i = 0; i < CONNECTION_SIZE; i++) {
+//		if (from->connections[i] == 0) {
+//			from->connections[i] = to;
+//		}
+//	}
+//}
+
+
+int isSpace(int c) {
+	return c == ' ' || c == '\t';
 }
 
-void print(node *l) {
-	printf("/////////////////////\n");
-	while (l != 0) {
-		printf("%d\n", l->value);
-		l = l->next;
-	}
-	printf("/////////////////////\n");
+int isAlphaNum(int c) 
+{
+	return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
 }
 
-node *newNode(int value) {
-	node *n = (node *)malloc(sizeof(node));
-	n->value = value;
-	n->next = 0;
-	return n;
-}
-
-// Does not keep the order
-node *copy(node *l) {
-	node *newN = newNode(l->value);
-	l = l->next;
-	while (l != 0) {
-		newN = append(newN, newNode(l->value));
-		l = l->next;
-	}
-	return newN;
-}
-
-node *merge(node *l, node *ll) {
-	node *p = l;
-	while (p->next != 0) {
-		p = p->next;
-	}
-	p->next = ll;
-	return l;
-}
-
-node *split(node *l, int where) {
-	int i;
+// return the line pointer pointed at the end of the word
+char * getWord(char *word, char *line) {
+	int c = '\0';
 	
-	for (i = 0; i < (where - 1) && l != 0; i++){
-		l = l->next;
-	}
-
-	if (i == (where - 1)) {
-		node *newN = l->next;
-		l->next = 0;
-		return newN;
-	}
-	return 0;
+	while (isSpace((c = *line++)));
+	
+	char *wp = word;
+	
+	do {
+		*wp++ = (char)c;
+	} while (*line != EOF && *line != '\0' && isAlphaNum((c = *line++)));
+	
+	*wp = '\0';
+	
+	return line;
 }
 
-void insertAfter(node *l, node *toAdd, int valueAfter) {
-	while (l != 0) {
-		if (l->value == valueAfter) {
-			toAdd->next = l->next;
-			l->next = toAdd;
-			break;
+struct connection {
+	char from[10];
+	char to[10];
+};
+connection connections[100] = {};
+int cIdx = 0;
+
+int count(char *from) {
+	int result = 0;
+	for (int i = 0; i < cIdx; i++) {
+		if (strcmp(connections[i].from, from) == 0) {
+			result++;
 		}
-		l = l->next;
 	}
+	return result;
 }
 
-void insertBefore(node *l, node *toAdd, int valueBefore) {
-	node *previous = l;
-	while (l != 0) {
-		if (l->value == valueBefore) {
-			toAdd->next = previous->next;
-			previous->next = toAdd;
-			break;
+connection **find(char *from) {
+	connection **result = (connection **)malloc(count(from) * sizeof(connection *));
+	int rIdx = 0;
+	for (int i = 0; i < cIdx; i++) {
+		if (strcmp(connections[i].from, from) == 0) {
+			result[rIdx++] = &connections[i];
 		}
-		previous = l;
-		l = l->next;
 	}
+	return result;
 }
 
-
-node *reverse(node *l) {
-	node *previous = 0;
-	while (l != 0) {
-		node *next = l->next;
-		l->next = previous;
-		previous = l;
-		l = next;
-	}
-	return previous;
-}
-
-node *reverseRecursive(node *current, node *previous) {
-	node *next = current->next;
-	current->next = previous;
-	if (next == 0) {
-		return current;
-	}
-	return reverseRecursive(next, current);
-}
 
 int main() {
-	node *list = newNode(0);
+	FILE *f = fopen("input.txt", "r");
+	char buf[300];
+
 	
-	for (int i = 0; i < 10; i++) {
-		node *n = newNode(i);
-		list = append(list, n);
+	while (fgets(buf, 300, f)) {
+		char *line = buf;
+		connection c = connection{};
+		line = getWord(c.from, line);
+		line = getWord(c.to, line);
+		connections[cIdx++] = c;
 	}
-	node *ll = copy(list);
 	
-	list = merge(list, ll);
-	//print(list);
-	node *s = split(list, 10);
-	print(list);
-	node *reversed = reverse(list);
-	print(reversed);
-	node *reReversed = reverseRecursive(reversed, 0);
-	print(reReversed);
-	
-	
+	connection **startConnections = find("start");
+
+
+	//for (int i = 0; i < cIdx; i++) {
+	//	cave *n = find(connections[i].from);
+	//	if (n == 0) {
+	//		newcave(connections[i].from); // just create an entry with the cave
+	//	}
+	//}
+//
+	//for (int i = 0; i < cIdx; i++) {
+	//	cave *from = find(connections[i].from);
+	//	cave *to = find(connections[i].to);
+	//	addconnection(from, to);
+	//}
+//
+	//cave c = caves[0];
+	//for (int i = 0; c.connections[i] != 0; i++) {
+	//	printf("%s \n", c.connections[i]->id);
+	//}
+
+	//printf("from : %s to : %s\n", connections[i].from, connections[i].to); 
+	printf("OK\n");
 }
