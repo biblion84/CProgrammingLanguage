59010da8392b698291060c3252fae94e942f440f
Lucas <biblion84@users.noreply.github.com>
2021-12-14 10:34:31 +0100

    AOC 14 part 1

--- main.cpp
+++ main.cpp
@@ -1,209 +1,93 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
 
-// recursive exercises from https://www.w3resource.com/c-programming-exercises/recursion/index.php
+struct pair {
+	char from[3];
+	char to[2];
+};
 
-void count(int i) {
-	if (i == 0) {
-		return;
+char getPair(pair *pairs, int pairsLen, char a, char b) {
+	for (int i = 0; i < pairsLen; i++) {
+		if (pairs[i].from[0] == a && pairs[i].from[1] == b) {
+			return pairs[i].to[0];
+		}
 	}
-	count(i - 1);
-	printf("%d, ", i);
+	return '\0';
 }
 
-int add(int i) {
-	if (i == 1) {
-		return 1;
-	}
-	return i + add(i - 1);
-}
-
-int fibonnaci(int i) {
-	if (i <= 1) {
-		return 1;
-	}
-	return fibonnaci(i - 1) + fibonnaci(i - 2);
-}
-
-void arrayPrint(int *a, int i) {
-	if (i == 1) {
-		printf("%d", a[i]);
-		return;
-	}
-	arrayPrint(a, i - 1);
-	printf("%d", a[i]);
-}
-
-void countDigits(int n, int count) {
-	if ((n / 10) > 0) {
-		countDigits(n / 10, count + 1);
-	} else {
-		printf("%d digits\n", count);
-	}
-}
-
-int sumDigits(int n) {
-	if (n == 0) {
-		return 0;
-	}
-	int digit = n % 10;
-	return digit + sumDigits(n / 10);
-}
-
-int gcd(int a, int b) {
-	if (b == 0) {
-		return a;
-	}
-	a = a % b;
-	return gcd(b, a);
-}
-
-int maxArray(int *arr, int max, int depth) {
-	if (depth < 0) {
-		return max;
-	}
-	int newMax = max;
-	if (arr[depth] > newMax) {
-		newMax = arr[depth];
-	}
-	return maxArray(arr, newMax, depth - 1);
-}
-
-void reverse(char *s, int index) {
-	if (s[index] == '\0') {
-		return;
-	}
-	reverse(s, index + 1);
-	printf("%c", s[index]);
-}
-
-int factorial(int n) {
-	if (n == 1) {
-		return 1;
-	}
-	return n * factorial(n - 1);
-}
-
-int binary(int n, int power, int bin) {
-	if (n == 0) {
-		return bin;
-	}
-	int rest = n % 2;
-	bin = bin + (rest * power);
-	power *= 10;
-	return binary(n / 2, power, bin);
-}
-
-bool prime(int n, int divisor) {
-	if (divisor >= n) {
-		return true;
-	}
-	if (n % divisor == 0) {
-		return false;
-	}
-
-	return prime(n, divisor + 1);
-}
-
-int lcm(int a, int b, int amul, int bmul) {
-	if ((a * amul) == (b * bmul)) {
-		return a * amul;
-	} else if ((a * amul) > (b * bmul)) {
-		return lcm(a, b, amul, bmul + 1);
-	} else {
-		return lcm(a, b, amul + 1, bmul);
-	}
-}
-
-void odd(int n) {
-	if (n == 1) {
-		return;
-	}
-	odd(n - 1);
-	if (n % 2 == 0) {
-		printf("%d, ", n);
-	}
-}
-
-bool palindrome(char *s, int len, int current) {
-	int lastIndex = len - current;
-	if (current >= lastIndex) {
-		return true;
-	}
-	if (s[current] != s[lastIndex]) {
-		return false;
-	}
-	return palindrome(s, len, current + 1);
-}
-
-
-int power(int n, int pow) {
-	if (pow <= 0) {
-		return 1;
-	}
-	if (pow == 1) {
-		return n;
-	}
-	return n * power(n, pow - 1);
-}
-
-void hailstone(int n) {
-	printf("%d, ", n);
-	if (n == 1) {
-		return;
-	}
-	if (n % 2 == 0) {
-		hailstone(n / 2);
-	} else {
-		hailstone((n * 3) + 1);
-	}
-}
-
-void copy(char *in, char *out) {
-	if (*in == '\0') {
-		*out = '\0';
-		return;
-	}
-	*out = *in;
-	copy(in + 1, out + 1);
-}
-
-void firstCapital(char *s) {
-	if (*s == '\0') {
-		printf("No capital\n");
-		return;
-	}
-	if (*s >= 'A' && *s <= 'Z') {
-		printf("First capital : %c\n", *s);
-		return;
-	}
-	firstCapital(s + 1);
-}
-
-int binSearch(int *a, int search, int low, int high) {
-	if (low > high) {
-		return -1;
-	}
-	int middle = (low + high) / 2;
-	if (a[middle] == search) {
-		return middle;
-	} else if (a[middle] > search) {
-		return binSearch(a, search, low, middle - 1);
-	} else {
-		return binSearch(a, search, middle + 1, high);
-	}
-}
+const int LEN = 100;
 
 int main() {
-	
-	int a[10];
-	for (int i = 0; i < 10; i++) {
-		a[i] = i * 3;
-		//printf("%d\n", a[i]);
+	char *primary = (char *) malloc(10000 * sizeof(char));
+	char *secondary = (char *) malloc(10000 * sizeof(char));
+	for (int i = 0; i < 10000; i++) {
+		primary[i] = '\0';
+		secondary[i] = '\0';
 	}
-	
-	int idx = binSearch(a, 9, 0, 9);
-	printf("index : %d, %d", idx, a[idx]);
+	strcpy(primary, "SNPVPFCPPKSBNSPSPSOF");
 
+	FILE *f = fopen("input.txt", "r");
+	char buffer[300] = {};
+
+	pair pairs[LEN] = {};
+	int pairIdx = 0;
+
+	while (fgets(buffer, 300, f) != 0) {
+		char *line = buffer;
+		line = getWord(pairs[pairIdx].from, line);
+		
+		char temp[5];
+		line = getWord(temp, line); // remove ->
+		
+		line = getWord(pairs[pairIdx].to, line);
+
+		pairIdx++;
+	}
+
+	for (int step = 0; step < 10; step++) {
+		int sIdx = 0;
+		int i;
+		for (i = 0; primary[i + 1] != '\0'; i++) {
+			secondary[sIdx++] = primary[i];
+			char c = getPair(pairs, LEN, primary[i], primary[i + 1]);
+			if (c != '\0') {
+				secondary[sIdx++] = c;
+			}
+		}
+		secondary[sIdx++] = primary[i];
+		secondary[sIdx++] = '\0';
+
+		char *temp = primary;
+		primary = secondary;
+		secondary = temp;
+	}
+	printf("%s\n", primary);
+
+	int occurences[26] = {};
+	for (int i = 0; primary[i] != '\0'; i++) {
+		occurences[primary[i] - 'A']++;
+	}
+	int most = 0;
+	int least = 1  << 30;
+	for (int i = 0; i < 26; i++) {
+		if (occurences[i] == 0) {
+			continue;
+		}
+		if (occurences[i] > most) {
+			most = occurences[i];
+		}
+		if (occurences[i] < least) {
+			least = occurences[i];
+		}
+	}
+	printf("%d : %d\n", most, least);
+	printf("result :  %d\n", most - least);
+
+	//for (int i = 0; i < pairIdx; i++) {
+	//	printf("-%s- -> -%s-\n", pairs[i].from, pairs[i].to);
+	//}
 
 	
 	printf("\nOK\n");
